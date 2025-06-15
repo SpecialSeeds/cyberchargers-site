b912e20e3fd529ed4cf638b6114bd2153703875e
fiddlyfiddler <elvin.duong@gmail.com>
2025-06-15 18:06:25 -0400

    added formatting to writeups page, added tjctf & pico pictures to /assets

--- src/app/writeups/page.tsx
+++ src/app/writeups/page.tsx
@@ -1,19 +1,39 @@
+import Image from "next/image";
 export default function WriteupsPage() {
   return (
     <main className="min-h-screen p-8 bg-black text-white">
       <h1 className="text-4xl font-bold text-cyan-400">CTF Writeups</h1>
-      <ul className="mt-6 space-y-4">
-        <li>
-          <a href="/writeups/chall1" className="text-lg text-cyan-300 hover:underline">
+      <ul className="mt-6 space-y-4 flex-col">
+        <li className="flex bg-blue-700">
+          <a
+            href="/writeups/chall1"
+            className="text-lg text-cyan-300 hover:underline"
+          >
             chall1
           </a>
+          <Image
+            src="/assets/tjctf.png"
+            alt="tjctf"
+            width={50}
+            height={50}
+            className="ml-auto"
+          />
         </li>
-        <li>
+
+        <li className="flex bg-red-700">
           <a href="/writeups/chall2" className="text-lg text-cyan-300 hover:underline">
             chall2
           </a>
+          <Image
+            src="/assets/picoctf.jpeg"
+            alt="picoctf"
+            width={50}
+            height={50}
+            className="ml-auto"
+          />
         </li>
       </ul>
     </main>
   );
 }
+
