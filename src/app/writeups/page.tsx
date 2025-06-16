c33b33ac1af12b164d44f48d6c4c4efd19a8bee0
fiddlyfiddler <elvin.duong@gmail.com>
2025-06-15 20:24:14 -0400

    attempting to implement header part by part now, part 1

--- src/app/writeups/page.tsx
+++ src/app/writeups/page.tsx
@@ -7,13 +7,22 @@ export default function WriteupsPage() {
       <h1 className="text-4xl font-bold text-cyan-400">CTF Writeups</h1>
       <ul className="mt-6 space-y-4 flex-col w-1/2">
         <li className="flex bg-[#1f1f1f]">
-          <a
-            href="/writeups/chall1"
-            className="text-lg text-cyan-300 hover:underline"
-          >
-            TJCTF 2025
-          </a>
-          <Image
+          <ul className="flex-col space-y-1 ">
+            <li><a
+              href="/"
+              className="text-lg text-cyan-300 hover:underline"
+            >
+              TJCTF 2025
+            </a></li>
+            <li>
+            <a
+              href="/"
+              className="text-sm text-cyan-300 hover:underline"
+            >
+              TJCTF was one of the competitions of all time. definitely a
+              shocker
+            </a></li>
+          </ul><Image
             src={tj}
             alt="tjctf"
             width={50}
