1bebed435f8f121601049b1500d8a873c1905b83
fiddlyfiddler <elvin.duong@gmail.com>
2025-06-15 20:18:57 -0400

    revert

--- src/app/writeups/page.tsx
+++ src/app/writeups/page.tsx
@@ -1,48 +1,33 @@
-
 import Image from "next/image";
+import tj from "@/assets/tjctf.png";
+import picoImg from "@/assets/picoctf.jpeg";
 export default function WriteupsPage() {
   return (
     <main className="min-h-screen p-8 bg-black text-white">
       <h1 className="text-4xl font-bold text-cyan-400">CTF Writeups</h1>
       <ul className="mt-6 space-y-4 flex-col w-1/2">
         <li className="flex bg-[#1f1f1f]">
-          <ul className="flex-col space-y-1 ">
-            <li><a
-              href="/"
-              className="text-lg text-cyan-300 hover:underline"
-            >
-              TJCTF 2025
-            </a></li>
-            <li>
-            <a
-              href="/"
-              className="text-sm text-cyan-300 hover:underline"
-            >
-              TJCTF was one of the competitions of all time. definitely a
-              shocker
-            </a></li>
-          </ul>
+          <a
+            href="/writeups/chall1"
+            className="text-lg text-cyan-300 hover:underline"
+          >
+            TJCTF 2025
+          </a>
           <Image
-            src="/assets/tjctf.png"
+            src={tj}
             alt="tjctf"
-            width={60}
-            height={60}
+            width={50}
+            height={50}
             className="ml-auto"
           />
         </li>
 
         <li className="flex bg-[#1f1f1f]">
-          <ul className="flex-col space-y-1 ">
-            <li><a href="/" className="text-lg text-cyan-300 hover:underline">
-              PicoCTF
-            </a></li>
-            
-            <li><a href="/" className="text-sm text-cyan-300 hover:underline">
-              checkout my writeups at fiddlyfiddler.gitbook.io!!
-            </a></li>
-          </ul>
+          <a href="/writeups/chall2" className="text-lg text-cyan-300 hover:underline">
+            chall2
+          </a>
           <Image
-            src="assets/picoctf.jpeg"
+            src={picoImg}
             alt="picoctf"
             width={50}
             height={50}
@@ -53,4 +38,5 @@ export default function WriteupsPage() {
     </main>
   );
 }
+
 
